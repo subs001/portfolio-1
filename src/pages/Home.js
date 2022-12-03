43335d6351eb7a479b0db06d8865dabe70d0a34e
CHN-Subramanian <subramanian.venkittanarayanan@arcadia.com>
2022-12-04 01:48:42 +0530

    Add tailwind, Add Home page & route, Add Profile comp

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Profile from "../components/Profile.component";
+
+function Home() {
+  return (
+    <div>
+      Home
+      <Profile></Profile>
+    </div>
+  );
+}
+
+export default Home;
