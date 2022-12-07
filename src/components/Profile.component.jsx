c22127341e75eb358412da4bd36c0646e11eaa46
CHN-Subramanian <subramanian.venkittanarayanan@arcadia.com>
2022-12-07 23:09:40 +0530

    Made homepage responsive, add typewrite animation

--- src/components/Profile.component.jsx
+++ src/components/Profile.component.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import profile_pic from "../assets/images/profile-pic.jpg";
 import SmallText from "./SmallText.component";
-import {GiPaintBrush, GiForkKnifeSpoon} from 'react-icons/gi';
-import {BsLaptop} from 'react-icons/bs';
+import { GiPaintBrush, GiForkKnifeSpoon } from "react-icons/gi";
+import { BsLaptop } from "react-icons/bs";
+import "../styles/typewriter.css";
+
 
 function Profile() {
   return (
@@ -14,8 +16,10 @@ function Profile() {
           className="rounded-full w-64 h-auto align-middle border-fuchsia-300 border-4 shadow-md shadow-[#FFCACA]"
         />
       </div>
-      <p className="font-marckScript text-5xl flex justify-center py-24">Hi, I'm Shriya Rathor</p>
-      <ul className="grid grid-cols-3 px-32 py-16 gap-4">
+      <p className="font-marckScript text-4xl md:text-5xl text-center my-16 mx-auto md:my-24 whitespace-nowrap w-[21ch] overflow-hidden line">
+        Hi, I'm Shriya Rathor
+      </p>
+      <ul className="grid grid-rows-3 md:grid-cols-3 px-32 py-8 md:pt-8 gap-16 md:gap-4">
         <SmallText text={"Artist"} icon={<GiPaintBrush />}></SmallText>
         <SmallText text={"Engineer"} icon={<BsLaptop />}></SmallText>
         <SmallText text={"Foodie"} icon={<GiForkKnifeSpoon />}></SmallText>
--- src/components/SmallText.component.jsx
+++ src/components/SmallText.component.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-function SmallText({ text, icon}) {
-  return <li className="font-satisfy text-[#22A39F] flex justify-center text-3xl gap-4">{text}{icon}</li>;
+function SmallText({ text, icon }) {
+  return (
+    <li className="font-satisfy text-[#22A39F] flex justify-center text-3xl gap-4 drop-shadow-lg shadow-gray-500 transform transition duration-100 hover:scale-150 hover:text-teal-500">
+      {text}
+      {icon}
+    </li>
+  );
 }
 
 export default SmallText;
