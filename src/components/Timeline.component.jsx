3286b7bab0f8b2201f1ae6a5c3e53fbb3b82576c
CHN-Subramanian <subramanian.v2k@gmail.com>
2022-12-12 18:43:28 +0530

    Change timecline card bg color, change mobile nav bg color

--- src/components/Timeline.component.jsx
+++ src/components/Timeline.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function TimelineLeft({ job }) {
   return (
     <div className="flex flex-row-reverse md:contents">
-      <div className="bg-[#142F43] col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md w-full md:w-[45%] hover:scale-110 duration-150">
+      <div className="bg-[#411530] col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md w-full md:w-[45%] hover:scale-110 duration-150">
         <h3 className="font-semibold text-lg text-[#FF87CA] font-quicksand">
           {job.companyName}
         </h3>
@@ -23,9 +23,9 @@ function TimelineLeft({ job }) {
       </div>
       <div className="col-start-5 col-end-6 md:mx-auto relative mr-10">
         <div className="h-full w-6 flex items-center justify-center">
-          <div className="h-full w-1 bg-[#142F43] pointer-events-none"></div>
+          <div className="h-full w-1 bg-[#411530] pointer-events-none"></div>
         </div>
-        <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-[#AAC4FF] shadow"></div>
+        <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-[#B68973] shadow"></div>
       </div>
     </div>
   );
@@ -36,11 +36,11 @@ function TimelineRight({ job }) {
     <div className="flex md:contents">
       <div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
         <div className="h-full w-6 flex items-center justify-center">
-          <div className="h-full w-1 bg-[#142F43] pointer-events-none"></div>
+          <div className="h-full w-1 bg-[#411530] pointer-events-none"></div>
         </div>
-        <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-[#AAC4FF]"></div>
+        <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-[#B68973]"></div>
       </div>
-      <div className="bg-[#142F43] col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md w-full md:w-[45%] hover:scale-110 duration-150">
+      <div className="bg-[#411530] col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md w-full md:w-[45%] hover:scale-110 duration-150">
         <h3 className="font-semibold text-lg mb-1 text-[#BCE29E] font-quicksand">
           {job.companyName}
         </h3>
