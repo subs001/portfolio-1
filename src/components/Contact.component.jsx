09970b082733d5d40455feccb3808e58ff2e786e
CHN-Subramanian <subramanian.v2k@gmail.com>
2022-12-12 13:16:26 +0530

    Add socials section, add icons, add coinflip animation, add resume download, made responsive

--- src/components/Contact.component.jsx
+++ src/components/Contact.component.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import Header from "./Header.component";
+import { AiFillLinkedin, AiFillInstagram, AiFillGithub } from "react-icons/ai";
+import { IoDocumentTextSharp } from "react-icons/io5";
+import "../styles/coinflip.css";
 
 function Contact() {
+  const iconSize = 80;
+  const iconStyle = "m-4 text-[#411530] scale-[62.5%] md:scale-100 drop-shadow-lg";
+
   return (
     <div name="contact" id="contact">
       <Header text={"Contact"} />
@@ -31,15 +37,53 @@ function Contact() {
             placeholder="Enter your message"
             className="bg-[#411530] focus:bg-[#772658] focus:scale-105 duration-100 rounded-lg text-slate-200 placeholder:text-slate-200 px-4 py-2 w-[75%]"
           ></textarea>
-          <button type="submit" className="bg-[#B68973] hover:bg-[#816151] px-4 py-1 m-6 rounded-lg text-slate-200">
+          <button
+            type="submit"
+            className="bg-[#B68973] hover:bg-[#816151] px-4 py-1 m-6 rounded-lg text-slate-200 shadow-md"
+          >
             Send
           </button>
         </form>
       </div>
       <div className="w-full md:w-[50%] p-4 md:p-12 h-[35rem] float-none md:float-right">
-      <p className="font-poppins w-[75%] mx-auto my-4">
+        <p className="font-poppins w-[75%] mx-auto my-4 text-center">
           ...or on one of my Socials!
         </p>
+        <div className="flex flex-row w-full justify-center gap-4 md:gap-16 my-16">
+          <div className="icon">
+            <a href="">
+              <AiFillGithub
+                size={iconSize}
+                className={iconStyle}
+              ></AiFillGithub>
+            </a>
+          </div>
+          <div className="icon">
+            <a href="https://www.instagram.com/shriyaarathor/">
+              <AiFillInstagram
+                size={iconSize}
+                className={iconStyle}
+              ></AiFillInstagram>
+            </a>
+          </div>
+          <div className="icon">
+            <a href="https://www.linkedin.com/in/shriya-rathor-0b42851ba/">
+              <AiFillLinkedin
+                size={iconSize}
+                className={iconStyle}
+              ></AiFillLinkedin>
+            </a>
+          </div>
+        </div>
+        <p className="font-poppins w-[75%] mx-auto my-4 text-center">
+          Grab a copy of my resume here!
+        </p>
+        <a href="https://www.instagram.com/shriyaarathor/" className="flex justify-center mt-8  drop-shadow-lg">
+          <IoDocumentTextSharp
+            size={50}
+            className="text-[#B68973]"
+          ></IoDocumentTextSharp>
+        </a>
       </div>
     </div>
   );
